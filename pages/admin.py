802ac01fb6e8f0c8dc3ee0b8b9c608347750cfde
pysunita <sunitasahu.nl@gmail.com>
2021-11-11 13:09:27 +0100

    admin filterset up

--- pages/admin.py
+++ pages/admin.py
@@ -6,6 +6,8 @@ class TeamAdmin(admin.ModelAdmin):
         return format_html('<img src="{}" width="40" style="border-radius:50px;"/>'.format(object.photo.url))
     thumbnail.short_description = 'photo'
     list_display = ('id','thumbnail','first_name','designation','created_date')
-    list_display_links = ('id','first_name')
+    list_display_links = ('id','thumbnail','first_name')
+    search_fields = ('first_name','last_name','designation')
+    list_filter = ('designation',)
      
 admin.site.register(Team,TeamAdmin)
