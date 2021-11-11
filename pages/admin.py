4b12edc28b0fb07063cbcd506433ace3d02b5483
pysunita <sunitasahu.nl@gmail.com>
2021-11-11 11:49:26 +0100

    admin page set up

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 from .models import Team
-admin.site.register(Team)
+from django.utils.html import format_html
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="40" style="border-radius:50px;"/>'.format(object.photo.url))
+    thumbnail.short_description = 'photo'
+    list_display = ('id','thumbnail','first_name','designation','created_date')
+    list_display_links = ('id','first_name')
+     
+admin.site.register(Team,TeamAdmin)
