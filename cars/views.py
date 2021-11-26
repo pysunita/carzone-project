8d1be5fe1c9a7d9ffcbc157a9917dfc7ea5d38b8
pysunita <sunitasahu.nl@gmail.com>
2021-11-26 12:05:35 +0100

    search page views written

--- cars/views.py
+++ cars/views.py
@@ -20,5 +20,41 @@ def car_detail(request, id):
     return render(request,'cars/car_detail.html', data)
 
 def search(request):
-    return render(request, 'cars/search.html')
+    cars = Car.objects.order_by('-created_date')
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = cars.filter(description__icontains=keyword)
+
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:#if model not blank
+            cars = cars.filter(model__iexact=model)#iexact for the exact value
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            cars = cars.filter(city__iexact=city)
+
+    if 'year' in request.GET:
+        year = request.GET['year']
+        if year:
+            cars = cars.filter(year__iexact=year)
+
+    if 'body_style' in request.GET:
+        body_style = request.GET['body_style']
+        if body_style:
+            cars = cars.filter(body_style__iexact=body_style)
+
+    if 'min_price' in request.GET:
+        min_price = request.GET['min_price']
+        max_price = request.GET['max_price']
+        if max_price:
+            cars = cars.filter(price__gte=min_price, price__lte=max_price)
+
+    data = {
+        'cars': cars,
+    }
+
+    return render(request, 'cars/search.html', data)
 
