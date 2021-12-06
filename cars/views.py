6fe5c6c9ce81e63b2b2c1a29042e5d80d395534e
pysunita <sunitasahu.nl@gmail.com>
2021-12-06 13:19:42 +0100

    created admin folder inside template folder

--- cars/views.py
+++ cars/views.py
@@ -7,8 +7,18 @@ def cars(request):
     paginator = Paginator(cars, 2)
     page = request.GET.get('page')
     paged_cars = paginator.get_page(page)
+
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+
     data = {
         'cars': paged_cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
     }
     return render(request,'cars/cars.html', data)
 
@@ -21,6 +31,11 @@ def car_detail(request, id):
 
 def search(request):
     cars = Car.objects.order_by('-created_date')
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    transmission_search = Car.objects.values_list('transmission', flat=True).distinct()
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
@@ -54,6 +69,11 @@ def search(request):
 
     data = {
         'cars': cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
+        'transmission_search': transmission_search,
     }
 
     return render(request, 'cars/search.html', data)
